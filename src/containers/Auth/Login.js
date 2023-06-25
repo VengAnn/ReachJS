e450ca6d3ce66a1fe5438872c61c39df8a50c16f
VengAnn <venganncoco@gmail.com>
2023-06-25 16:01:06 +0700

    update

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -19,13 +19,29 @@ class Login extends Component {
             <div className="login-background">
                 <div className='login-container'>
                     <div className="login-content row">
-                        <div className="col-12 text-center">Login</div>
-                        <div className="col-12 form group">
+                        <div className="col-12 text-login">Login</div>
+                        <div className="col-12 form group login-input">
                             <label >Username</label>
-                            <input type="text" className='form-control' />
+                            <input type="text" className='form-control' placeholder="Enter your username" />
+                        </div>
+                        <div className="col-12 form group login-input">
+                            <label >Password:</label>
+                            <input type="text" className='form-control' placeholder="Enter you password" />
+                        </div>
+                        <div className="col-12">
+                            <button className="btn-login">Login</button>
+                        </div>
+                        <div className="col-12">
+                            <span className="forgot-password">Forgot your password</span>
+                        </div>
+                        <div className="col-12 text-center mt-3">
+                            <span className="text-other-login">Or Login with</span>
+                        </div>
+                        <div className="col-12 social-login">
+                            <i className="fab fa-google-plus-g google"></i>
+                            <i className="fab fa-facebook-f facebook"></i>
                         </div>
                     </div>
-
                 </div>
             </div>
         )
