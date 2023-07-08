acaf4f041313cf1839e1abe1460b90dbc22fc1b3
VengAnn <venganncoco@gmail.com>
2023-07-08 20:54:44 +0700

    new update

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/bookingcare-2020.svg';
+import { FormattedMessage } from 'react-intl';
 
 class HomeHeader extends Component {
 
@@ -18,24 +19,24 @@ class HomeHeader extends Component {
                         </div>
                         <div className="center-content">
                             <div className="child-content">
-                                <div><b>Chuyển khoa</b></div>
-                                <div className="subs-title">Tìm bác sĩ theo chuyên khoa </div>
+                                <div><b> <FormattedMessage id="homeheader.speciality" /></b></div>
+                                <div className="subs-title"><FormattedMessage id="homeheader.searchdoctor" /></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Cơ Sở y tế</b></div>
-                                <div className="subs-title">Chọn bẹnhe viện phòng khám</div>
+                                <div><b><FormattedMessage id="homeheader.health-facility" /></b></div>
+                                <div className="subs-title"><FormattedMessage id="homeheader.select-room" /></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Bác sĩ</b></div>
-                                <div className="subs-title">Chọn Bác sĩ giỏi</div>
+                                <div><b><FormattedMessage id="homeheader.doctor" /></b></div>
+                                <div className="subs-title"><FormattedMessage id="homeheader.select-doctor" /></div>
                             </div>
                             <div className="child-content">
-                                <div><b>Gói khám</b></div>
-                                <div className="subs-title">Khám sức khỏe tổng quất</div>
+                                <div><b><FormattedMessage id="homeheader.fee" /></b></div>
+                                <div className="subs-title"><FormattedMessage id="homeheader.check-health" /></div>
                             </div>
                         </div>
                         <div className="right-content">
-                            <div className="support"><i className="far fa-question-circle"></i> Hỗ trợ</div>
+                            <div className="support"><i className="far fa-question-circle"></i> <FormattedMessage id="homeheader.support" /></div>
                             <div className="language-vi">VN</div>
                             <div className="language-en">EN</div>
                         </div>
@@ -43,8 +44,8 @@ class HomeHeader extends Component {
                 </div>
                 <div className="home-header-banner">
                     <div className="content-up">
-                        <div className="title1">NỀN TẢNG Y TẾ</div>
-                        <div className="title2">CHĂM SÓC SỨC KHỎE TOÀN DIỆN</div>
+                        <div className="title1"><FormattedMessage id="banner.title1" /></div>
+                        <div className="title2"><FormattedMessage id="banner.title2" /></div>
                         <div className="search">
                             <i className="fas fa-search"></i>
                             <input type="text" placeholder="Tìm Chuyên khoa khám bệnh" />
@@ -55,27 +56,27 @@ class HomeHeader extends Component {
                         <div className="options">
                             <div className="option-child">
                                 <div className="icon-child"><i className="far fa-hospital"></i></div>
-                                <div className="text-child">Khám chuyên khoa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child1" /></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><i className="fas fa-mobile"></i></div>
-                                <div className="text-child">Khám từ xa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child2" /></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><i className="fas fa-stethoscope"></i></div>
-                                <div className="text-child">Khám tổng quất</div>
+                                <div className="text-child"> <FormattedMessage id="banner.child3" /></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><i className="fa fa-plus-square" aria-hidden="true"></i></div>
-                                <div className="text-child">Xết nghiệm y học</div>
+                                <div className="text-child"><FormattedMessage id="banner.child4" /></div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><i className="fa fa-user-md" aria-hidden="true"></i></div>
-                                <div className="text-child">Sức khỏe tính thần</div>
+                                <div className="text-child"><FormattedMessage id="banner.child5" />    </div>
                             </div>
                             <div className="option-child">
                                 <div className="icon-child"><i className="fas fa-procedures"></i></div>
-                                <div className="text-child">Khám nha khoa</div>
+                                <div className="text-child"><FormattedMessage id="banner.child6" /></div>
                             </div>
                         </div>
                     </div>
